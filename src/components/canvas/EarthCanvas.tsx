594fce83a287c6c662495f63c94d1da25e92d17e
hassan-dev51 <hassanmunir363@gmail.com>
2023-03-28 23:06:19 +0500

    completed

--- src/components/canvas/EarthCanvas.tsx
+++ src/components/canvas/EarthCanvas.tsx
@@ -6,7 +6,7 @@ import Loader from "../Loader";
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={2} position-y={0} rotation-y={0} />
   );
 };
 
--- src/components/canvas/StarCanvas.tsx
+++ src/components/canvas/StarCanvas.tsx
@@ -1,14 +1,45 @@
-import React from "react";
+"use client";
+import React, { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Preload, Points, PointMaterial } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
 
-const Star = () => {
+const Star = (props: any) => {
+  const ref = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
   return (
-    <div>
-      <h1>starts</h1>
-    </div>
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
   );
 };
 const StarCanvas = () => {
-  return <div>StarCanvas</div>;
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Star />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
 };
 
 export default StarCanvas;
